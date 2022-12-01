0900efda93d7be533b4f97483033221ae50eacd0
Bonganisandhya-07 <112746880+Bonganisandhya-07@users.noreply.github.com>
2022-12-01 13:51:34 +0530

    Add files via upload

--- JdbcConnection.java
+++ JdbcConnection.java
@@ -1,18 +1,21 @@
 package com.exercise.exercise.exercise1;
 import java.sql.*;
 import java.util.Scanner;
-
+import java.util.Scanner;
 
 public class JdbcConnection {
 	static final Scanner obj=new Scanner(System.in);
 	public static void update(Connection connection,int software_id) throws SQLException {
-		PreparedStatement pstmt = connection.prepareStatement("update  softwaredetails set  stocks=? where software_id="+software_id);
-		pstmt.setInt(1,150);
-		
-		pstmt.executeUpdate();
-		
+		PreparedStatement pstmt = connection.prepareStatement("update  softwaredetails set  softwarename=? where software_id=?");
+		String value=obj.next();
+		pstmt.setString(1,value);
+		pstmt.setInt(2,software_id);
+
+		int record=pstmt.executeUpdate();
+		System.out.println(record);
+
 	}
-	
+
 	public static void addSoftwareDetails(Connection connection,SoftwareDetails  obj) throws SQLException {
 		PreparedStatement pstmt = connection.prepareStatement("insert into softwaredetails values(?,?,?,?,?,?,?) ");
 		pstmt.setString(1,obj.getsoftwarename());
@@ -22,12 +25,36 @@ public class JdbcConnection {
 		pstmt.setInt(5, obj.getrating());
 		pstmt.setString(6,obj.getcompanyname());
 		pstmt.setInt(7, obj.getstock());
-				
+
 		int record=pstmt.executeUpdate();
 		System.out.println(record);
-	
+
 	}
+	public static void delete(Connection connection,int software_id) throws SQLException {
+		PreparedStatement pstmt = connection.prepareStatement("delete from softwaredetails where price=?");
+		int val=obj.nextInt();
+		pstmt.setInt(1, val);
+		int record=pstmt.executeUpdate();
+		System.out.println(record);
+
+	}
+	public static void view(Connection connection,int software_id) throws SQLException {
+		PreparedStatement pstmt = connection.prepareStatement("select * from softwaredetails where software_id=?");
+		int id=obj.nextInt();
+		pstmt.setInt(1,id);
+
+
+		ResultSet record=pstmt.executeQuery();
+while(record.next()) {
+			System.out.println(record.getString(1)+" "+record.getInt(2)+" "+record.getString(3)+" "+record.getInt(4)+" "+record.getInt(5)+" "+record.getString(6)+" "+record.getInt(7));
+
+		}
+
+}
 	
+
+
+
 	public static void main(String[] args)throws SQLException, Exception{
 		Connection connection=null;
 		final  SoftwareDetails addobj =new SoftwareDetails();
@@ -35,39 +62,44 @@ public class JdbcConnection {
 		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root" ,"6301269746");
 		System.out.println("enter 1 for addsoftwaredetails");
 		System.out.println("enter 2 for updatesoftware");
-		System.out.println("enter 3 for viewsoftware");
-		System.out.println("enter 4 for deletesoftware");
-		//static final Scanner obj=new Scanner(System.in);
+		System.out.println("enter 3 for deletesoftware");
+		System.out.println("enter 4 for viewsoftware");
 		System.out.println("enter your choice");
 		int choice = obj.nextByte();
 		switch(choice) {
-		case 1:
-			SoftwareDetails obj =  getSoftwareDetails ();
-			if(connection!=null) {
-			addSoftwareDetails(connection,obj);//method call
-			}
-			else {
-				System.out.println("check your connection");
-			}
-			
-			
-			break;
-		case 2:
-	
-			//SoftwareDetails obj1=getupdate();
-			update(connection,125);
-			
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		
-		default:
-			System.out.println("enter proper choice");
-			connection.close();
-		
-		
+			case 1:
+				SoftwareDetails obj =  getSoftwareDetails ();
+				if(connection!=null) {
+					addSoftwareDetails(connection,obj);//method call
+				}
+				else {
+					System.out.println("check your connection");
+				}
+
+
+				break;
+			case 2:
+
+				System.out.println("enter which record do you want to update");
+				update(connection,125);
+
+				break;
+			case 3:
+				System.out.println("enter which record do you want to delete");
+				delete(connection,125);
+
+				break;
+			case 4:
+				System.out.println("enter id to read the record");
+				view(connection,145);
+
+				break;
+
+			default:
+				System.out.println("enter proper choice");
+				connection.close();
+
+
 		}
 
 	}
@@ -91,12 +123,10 @@ public class JdbcConnection {
 
 	}
 
-		 
-		
-	}
-	
-	
-		
+
+
+}
+
 
 
 
